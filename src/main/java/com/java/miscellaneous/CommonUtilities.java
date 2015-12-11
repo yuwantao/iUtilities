b7e184fd3c8625c74b7e3c29fe8f848318ba881e
yuwantao <yuwantao1109@gmail.com>
2015-12-11 12:41:35 +0800

    initial commit.

---
+++ src/main/java/com/java/miscellaneous/CommonUtilities.java
@@ -0,0 +1,17 @@
+package com.java.miscellaneous;
+
+/**
+ * Created by yuwt on 2015/12/11.
+ *
+ * @author yuwt
+ */
+public class CommonUtilities {
+    /**
+     * Return the current working directory.
+     *
+     * @return the current working directory
+     */
+    public static String getCurrentWorkingDir() {
+        return System.getProperty("user.dir");
+    }
+}
---
+++ src/test/java/com/java/miscellaneous/CommonUtilitiesTest.java
@@ -0,0 +1,25 @@
+package com.java.miscellaneous;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by yuwt on 2015/12/11.
+ */
+public class CommonUtilitiesTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetCurrentWorkingDir() throws Exception {
+        String curWorkingDir = System.getProperty("user.dir");
+        assertEquals(curWorkingDir, CommonUtilities.getCurrentWorkingDir());
+    }
+}
